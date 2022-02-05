8fc89718a1d4299dab32aa986a2078bd8e37ea17
DragonWF <plarisanmarc@gmail.com>
2022-02-06 03:07:37 +0800

    Added main command structure and code (Still Bug present: Sniper not storing messages)

--- src/utils/processor.js
+++ src/utils/processor.js
@@ -1,10 +1,38 @@
+const MessageSniper = require("../commands/snipe");
+const HelpCommand = require("../commands/help");
+
+const help = new HelpCommand();
+
 class CommandProcessor {
-  processCommand(command) {
+  constructor(Discord) {
+    this.Discord = Discord;
+    this.sniper = new MessageSniper();
     this.commands = [
-      ["help", undefined],
-      ["snipe", undefined],
-      ["esnipe", undefined],
+      ["help", help.getCommandList, "none"],
+      ["snipe", this.sniper.snipeDeletedMessage, "embed"],
+      ["esnipe", this.sniper.snipeEditedMessage, "embed"],
     ];
+  }
+
+  configureSniper(message, type) {
+    if (type === "deletedMessage") this.sniper.storeDeletedMessage(message);
+    else this.sniper.storeOriginalMessage(message);
+    this.commands[1][1] = this.sniper.snipeDeletedMessage;
+    this.commands[2][1] = this.sniper.snipeEditedMessage;
+  }
+
+  processCommand(command, prefix) {
+    const [commandName, ...args] = command.content
+      .trim()
+      .substring(prefix.length)
+      .split(/\s+/);
+    const commandList = this.commands;
+    for (let cmd of commandList) {
+      if (commandName === cmd[0]) {
+        if (cmd[2] === "embed") return cmd[1](this.Discord);
+        return cmd[1]();
+      }
+    }
   }
 }
 
