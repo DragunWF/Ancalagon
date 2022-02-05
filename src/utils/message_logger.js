326d47562f4736e0db0bcd861cd45b6b7aa60cce
DragonWF <plarisanmarc@gmail.com>
2022-02-05 23:17:38 +0800

    Added basic message logging

--- src/utils/message_logger.js
+++ src/utils/message_logger.js
@@ -1,13 +1,31 @@
+const fs = require("fs");
+
 class MessageLogger {
-  logMessage(message) {
-    console.log("message logged");
+  logMessage(content, author) {
+    const log = `[${author}]: ${content}`;
+    console.log(log);
+    fs.appendFile("./data/logs/chat_logs.txt", `${log}\n`, (err) => {
+      if (err) console.log(error);
+    });
   }
 
-  logDeletedMessage(message) {
-    console.log("deleted message logged");
+  logDeletedMessage(content, author) {
+    const log = `Deleted Message:\n[${author}]: ${content}`;
+    console.log(log);
+    fs.appendFile("./data/logs/deleted_logs.txt", `${log}\n`, (err) => {
+      if (err) console.log(err);
+    });
   }
 
-  logEditedMessage(before, after) {
-    console.log("edited message logged");
+  logEditedMessage(before, after, author) {
+    const log = `Message Edit Event: (By: ${author})
+    Before: ${before}
+    After: ${after}`;
+    console.log(log);
+    fs.appendFile("./data/logs/edited_logs.txt", `${log}\n`, (err) => {
+      if (err) console.log(err);
+    });
   }
 }
+
+module.exports = MessageLogger;
