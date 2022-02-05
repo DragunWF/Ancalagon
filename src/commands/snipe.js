c75a0b84b24bad0b955e554a7c2d7baca37584a8
DragonWF <plarisanmarc@gmail.com>
2022-02-06 00:00:23 +0800

    Added main code for message sniping

--- src/commands/snipe.js
+++ src/commands/snipe.js
@@ -1,7 +1,6 @@
 class MessageSniper {
   constructor() {
     this.originalMessage = undefined;
-    this.editedMessage = undefined;
     this.deletedMessage = undefined;
   }
 
@@ -9,12 +8,32 @@ class MessageSniper {
     this.deletedMessage = message;
   }
 
-  storeEditedMessage(message) {
-    this.editedMessage = message;
-  }
-
   storeOriginalMessage(message) {
     this.originalMessage = message;
+  }
+
+  snipeDeletedMessage() {
+    const responses = [
+      "There are no messages to snipe!",
+      "Unfortunately, there's no message to snipe.",
+      "Better luck next time, no message to snipe.",
+      "Well that's quite disappointing, there's no message to snipe.",
+      "Sadly, there's no message to snipe...",
+    ];
+    if (this.deletedMessage) return this.deletedMessage;
+    return responses[Math.floor(Math.random() * responses.length)];
+  }
+
+  snipeEditedMessage() {
+    const responses = [
+      "No edited message to snipe sadly.",
+      "There are no edited messages to snipe!",
+      "No message to esnipe.",
+      "Most unfortunate, there's no message to esnipe!",
+      "Seems like there's no edited message to snipe.",
+    ];
+    if (this.originalMessage) return this.originalMessage;
+    return responses[Math.floor(Math.random() * responses.length)];
   }
 }
 
