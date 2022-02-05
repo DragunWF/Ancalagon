f3dc53f4e4eea04739999d9d4bc99f70544a5664
DragonWF <plarisanmarc@gmail.com>
2022-02-05 23:19:50 +0800

    Added main message events and logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
 const discord = require("discord.js");
 const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const prefix = "$";
+require("dotenv").config();
+
+const MessageLogger = require("./utils/message_logger");
+const logger = new MessageLogger();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,16 +12,34 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (message.author.bot) return;
-  console.log(`[${message.author.tag}]: ${message.content}`);
+  logger.logMessage(message.content, message.author.tag);
 
   if (message.content.startsWith(prefix)) {
-    const [commandName, ...args] = message.content
-      .trim()
-      .substring(prefix.length)
-      .split(/\s+/);
-
-    if (commandName === "help") message.channel.send("**In construction**");
+    processCommand(message);
   }
 });
+
+client.on("messageDelete", (message) => {
+  if (message.author.bot) return;
+  logger.logDeletedMessage(message.content, message.author.tag);
+});
+
+client.on("messageUpdate", (oldMessage, newMessage) => {
+  if (message.author.bot) return;
+  logger.logEditedMessage(
+    oldMessage.content,
+    newMessage.content,
+    oldMessage.author.tag
+  );
+});
+
+function processCommand(command) {
+  const [commandName, ...args] = command.content
+    .trim()
+    .substring(prefix.length)
+    .split(/\s+/);
+
+  if (commandName === "help") command.channel.send("**In construction**");
+}
 
 client.login(process.env.TOKEN);
