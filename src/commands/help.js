388e110c7baeb45995323a21cd2bfa66e779374a
DragonWF <plarisanmarc@gmail.com>
2022-02-06 18:50:20 +0800

    Added embeds, command categories, and general commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,8 +5,31 @@ class HelpCommand extends Command {
     super();
   }
 
-  getCommandList() {
-    return "**In Construction**";
+  concatenateCommands(array, hasArgs, initial = null) {
+    let output = "";
+    for (let item of array) {
+      if (hasArgs) output += `- \`$${initial} ${item}\`\n`;
+      else output += `- \`$${item}\`\n`;
+    }
+    return output;
+  }
+
+  getHelpCommandList() {
+    const commands = ["general", "games", "memes", "economy"];
+    const embedOutput = new this.messageEmbed()
+      .setColor(this.getRandomEmbedColor())
+      .setTitle("Command Categories :ringed_planet:")
+      .setDescription(this.concatenateCommands(commands, true, "help"));
+    return { embeds: [embedOutput] };
+  }
+
+  getGeneralCommandList() {
+    const commands = ["info", "snipe", "esnipe"];
+    const embedOutput = new this.messageEmbed()
+      .setColor(this.getRandomEmbedColor())
+      .setTitle("General Command List :tools:")
+      .setDescription(this.concatenateCommands(commands, false));
+    return { embeds: [embedOutput] };
   }
 
   getGameCommandList() {
@@ -15,6 +38,24 @@ class HelpCommand extends Command {
 
   getMemeCommandList() {
     return "**In Construction**";
+  }
+
+  getEconomyCommandList() {
+    return "**In Construction**";
+  }
+
+  processHelpCommand(reference, args) {
+    switch (args[0]) {
+      case "general":
+        return reference.getGeneralCommandList();
+      case "games":
+        return reference.getGameCommandList();
+      case "memes" || "meme":
+        return reference.getMemeCommandList();
+      case "economy":
+        return reference.getEconomyCommandList();
+    }
+    return reference.getHelpCommandList();
   }
 }
 
