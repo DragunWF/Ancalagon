a70aa76a62f679b340b7fee8dd5ef5031e3bf395
DragonWF <plarisanmarc@gmail.com>
2022-02-06 16:29:31 +0800

    Finally fixed bug (Snipe commands not working properly)

--- src/utils/processor.js
+++ src/utils/processor.js
@@ -1,24 +1,23 @@
-const MessageSniper = require("../commands/snipe");
 const HelpCommand = require("../commands/help");
+const MessageSniper = require("../commands/snipe");
 
 const help = new HelpCommand();
+const sniper = new MessageSniper();
 
 class CommandProcessor {
   constructor(Discord) {
     this.Discord = Discord;
-    this.sniper = new MessageSniper();
     this.commands = [
-      ["help", help.getCommandList, "none"],
-      ["snipe", this.sniper.snipeDeletedMessage, "embed"],
-      ["esnipe", this.sniper.snipeEditedMessage, "embed"],
+      ["help", help.getCommandList, true],
+      ["snipe", sniper.snipeDeletedMessage, false],
+      ["esnipe", sniper.snipeEditedMessage, false],
     ];
+    this.snipeCommands = [this.commands[1][0], this.commands[2][0]];
   }
 
   configureSniper(message, type) {
-    if (type === "deletedMessage") this.sniper.storeDeletedMessage(message);
-    else this.sniper.storeOriginalMessage(message);
-    this.commands[1][1] = this.sniper.snipeDeletedMessage;
-    this.commands[2][1] = this.sniper.snipeEditedMessage;
+    if (type === "deletedMessage") sniper.storeDeletedMessage(message);
+    else sniper.storeOriginalMessage(message);
   }
 
   processCommand(command, prefix) {
@@ -26,14 +25,18 @@ class CommandProcessor {
       .trim()
       .substring(prefix.length)
       .split(/\s+/);
-    const commandList = this.commands;
-    for (let cmd of commandList) {
+    for (let cmd of this.commands) {
       if (commandName === cmd[0]) {
-        if (cmd[2] === "embed") return cmd[1](this.Discord);
+        if (this.snipeCommands.includes(cmd[0])) cmd[1](sniper);
+        if (cmd[2]) return cmd[1](args);
         return cmd[1]();
       }
     }
+    return false;
   }
 }
 
 module.exports = CommandProcessor;
+
+// Commands list slot purposes
+// [commandName, commandFunction, doesCommandHaveArgs]
