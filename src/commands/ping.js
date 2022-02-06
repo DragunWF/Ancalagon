bd767c7299f4222d30a119e6c296ece3f11dc6ca
DragonWF <plarisanmarc@gmail.com>
2022-02-07 02:14:05 +0800

    Added base code for measuring ping

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,8 +5,25 @@ class PingCommand extends Command {
     super();
   }
 
-  getBotLatency(self) {
-    return;
+  getBotLatency(self, message) {
+    let embedOutput = new self.messageEmbed()
+      .setColor(self.getRandomEmbedColor())
+      .setAuthor({
+        name: "Pong!",
+      });
+    const responses = [
+      "Calculating ping...",
+      "Calculating latency...",
+      "Calculating speed...",
+    ];
+    const response = responses[Math.floor(Math.random() * responses.length)];
+    message.reply(response).then((result) => {
+      const ping = result.createdTimestamp - message.createdTimestamp;
+      embedOutput = embedOutput.setDescription(
+        `Ping: **${ping}ms** :ping_pong:`
+      );
+    });
+    return { embeds: [embedOutput] };
   }
 }
 
