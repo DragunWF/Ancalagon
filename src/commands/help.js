bca746a318d495a9e64c1d29d48f51fc87a4f4d1
DragonWF <plarisanmarc@gmail.com>
2022-02-06 16:35:02 +0800

    Added more boilerplates

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,7 +4,16 @@ class HelpCommand extends Command {
   constructor() {
     super();
   }
+
   getCommandList() {
+    return "**In Construction**";
+  }
+
+  getGameCommandList() {
+    return "**In Construction**";
+  }
+
+  getMemeCommandList() {
     return "**In Construction**";
   }
 }
