d591788d890a16a9ddcdb3fb5d375d01bfd724e3
DragonWF <plarisanmarc@gmail.com>
2022-02-06 12:16:54 +0800

    Added more embed colors and function to get random color

--- src/utils/command.js
+++ src/utils/command.js
@@ -1,6 +1,24 @@
 class Command {
   constructor() {
-    this.embedColors = ["#50C878", "#9b111e", "#9966cc", "#faed27", "#A1045A"];
+    this.embedColors = [
+      "#0F52BA", // Blue
+      "#9b111e", // Red
+      "#50C878", // Green
+      "#faed27", // Yellow
+      "#E38C2D", // Orange
+      "#A1045A", // Magneta
+      "#9966cc", // Amethyst
+      "#FFC0CB", // Pink
+      "#D5F6FB", // Aqua
+      "#9BD087", // Pastel Green
+      "#0000FF", // Cyan
+    ];
+  }
+
+  getRandomEmbedColor() {
+    return this.embedColors[
+      Math.floor(Math.random() * this.embedColors.length)
+    ];
   }
 }
 
