57b26085951dd7f2475007e983f60ee84bcb1500
DragonWF <plarisanmarc@gmail.com>
2022-02-06 20:53:10 +0800

    Added info command to the main processor

--- src/utils/processor.js
+++ src/utils/processor.js
@@ -1,7 +1,9 @@
 const HelpCommand = require("../commands/help");
+const InfoCommand = require("../commands/info");
 const MessageSniper = require("../commands/snipe");
 
 const help = new HelpCommand();
+const info = new InfoCommand();
 const sniper = new MessageSniper();
 
 class CommandProcessor {
@@ -11,6 +13,7 @@ class CommandProcessor {
       ["help", help.processHelpCommand, true, help],
       ["snipe", sniper.snipeDeletedMessage, false, sniper],
       ["esnipe", sniper.snipeEditedMessage, false, sniper],
+      ["info", info.getBotInformation, false, info],
     ];
     this.snipeCommands = [this.commands[1][0], this.commands[2][0]];
   }
@@ -28,8 +31,8 @@ class CommandProcessor {
     for (let cmd of this.commands) {
       if (commandName === cmd[0]) {
         if (this.snipeCommands.includes(cmd[0])) return cmd[1](sniper);
-        if (cmd[2]) return cmd[1](help, args);
-        return cmd[1]();
+        if (cmd[2]) return cmd[1](cmd[3], args);
+        return cmd[1](cmd[3]);
       }
     }
     return false;
