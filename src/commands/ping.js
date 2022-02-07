65bb905b9f89fc852a13ff76a13a71b924ebc14d
DragonWF <plarisanmarc@gmail.com>
2022-02-07 12:24:25 +0800

    Fixed embed output

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -6,23 +6,13 @@ class PingCommand extends Command {
   }
 
   getBotLatency(self, message) {
-    let embedOutput = new self.messageEmbed()
+    const ping = Math.abs(Date.now() - message.createdTimestamp);
+    const embedOutput = new self.messageEmbed()
       .setColor(self.getRandomEmbedColor())
       .setAuthor({
         name: "Pong!",
-      });
-    const responses = [
-      "Calculating ping...",
-      "Calculating latency...",
-      "Calculating speed...",
-    ];
-    const response = responses[Math.floor(Math.random() * responses.length)];
-    message.reply(response).then((result) => {
-      const ping = result.createdTimestamp - message.createdTimestamp;
-      embedOutput = embedOutput.setDescription(
-        `Ping: **${ping}ms** :ping_pong:`
-      );
-    });
+      })
+      .setDescription(`Ping: **${ping}ms** :ping_pong:`);
     return { embeds: [embedOutput] };
   }
 }
