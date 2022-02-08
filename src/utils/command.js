32f1d26804e4e7baf4d4e3d0959827964beefb48
DragonWF <plarisanmarc@gmail.com>
2022-02-08 12:32:52 +0800

    Turned embed colors into a private property

--- src/utils/command.js
+++ src/utils/command.js
@@ -1,26 +1,26 @@
 const { MessageEmbed } = require("discord.js");
+const embedMessageColors = [
+  "#0F52BA", // Blue
+  "#9b111e", // Red
+  "#50C878", // Green
+  "#faed27", // Yellow
+  "#E38C2D", // Orange
+  "#A1045A", // Magneta
+  "#9966cc", // Amethyst (Purple)
+  "#FFC0CB", // Pink
+  "#D5F6FB", // Aqua
+  "#9BD087", // Pastel Green
+  "#0000FF", // Cyan
+];
 
 class Command {
   constructor() {
-    this.messageEmbed = MessageEmbed;
-    this.embedColors = [
-      "#0F52BA", // Blue
-      "#9b111e", // Red
-      "#50C878", // Green
-      "#faed27", // Yellow
-      "#E38C2D", // Orange
-      "#A1045A", // Magneta
-      "#9966cc", // Amethyst (Purple)
-      "#FFC0CB", // Pink
-      "#D5F6FB", // Aqua
-      "#9BD087", // Pastel Green
-      "#0000FF", // Cyan
-    ];
+    this.MessageEmbed = MessageEmbed;
   }
 
   getRandomEmbedColor() {
     return this.embedColors[
-      Math.floor(Math.random() * this.embedColors.length)
+      Math.floor(Math.random() * embedMessageColors.length)
     ];
   }
 }
