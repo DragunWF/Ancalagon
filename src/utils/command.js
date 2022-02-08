f1f7f99640f1b52bd639366da5bf9423411f81b0
DragonWF <plarisanmarc@gmail.com>
2022-02-08 12:39:50 +0800

    Fixed private property bug

--- src/utils/command.js
+++ src/utils/command.js
@@ -19,7 +19,7 @@ class Command {
   }
 
   getRandomEmbedColor() {
-    return this.embedColors[
+    return embedMessageColors[
       Math.floor(Math.random() * embedMessageColors.length)
     ];
   }
