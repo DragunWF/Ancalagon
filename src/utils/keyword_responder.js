ffedd97cabb1dd463ffa6de1077d1b0471bf9abf
DragonWF <plarisanmarc@gmail.com>
2022-02-09 15:21:56 +0800

    Changed accessing data to the dot notation instead of the bracket notation

--- src/utils/keyword_responder.js
+++ src/utils/keyword_responder.js
@@ -4,26 +4,27 @@ const keywords = JSON.parse(fs.readFileSync("./data/bot/keywords.json"));
 
 class KeyWordResponder {
   static incrementTimesMentioned(data) {
-    keywords[keywords.indexOf(data)]["timesMentioned"]++;
+    keywords[keywords.indexOf(data)].timesMentioned++;
   }
 
   static pickRandomResponse(data) {
-    return data["responses"][Math.floor(Math.random() * keywords.length)];
+    return data.responses[Math.floor(Math.random() * data.keyword.length)];
   }
 
   static containsKeyword(string, data) {
-    if (Array.isArray(data["keyword"])) {
-      for (let word of string.toLowerCase().split(" "))
-        for (let keyword of data["keyword"])
+    string = string.toLowerCase().split(" ");
+    if (Array.isArray(data.keyword)) {
+      for (let word of string)
+        for (let keyword of data.keyword) 
           if (word === keyword) return true;
-    } else if (string === data["keyword"]) return true
+    } else if (string.includes(data.keyword)) return true;
     return false;
   }
 
   static checkMessage(message) {
     for (let data of keywords) {
       if (this.containsKeyword(message, data)) {
-        if (data["timesMentioned"] % data["timesToExecute"] === 0) {
+        if (data.timesMentioned % data.timesToExecute === 0) {
           this.incrementTimesMentioned(data);
           return this.pickRandomResponse(data);
         } else this.incrementTimesMentioned(data);
