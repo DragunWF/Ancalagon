a9f49b75151c3e58c84ddfa1ad3c758081666031
DragonWF <plarisanmarc@gmail.com>
2022-02-12 01:42:55 +0800

    Fixed response

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
 
 const server = express();
+const __dirname = path.resolve();
 
+server.use(express.static(path.join(__dirname, "src/server")));
 server.all("/", (request, response) => {
-  response.sendFile("page.html");
+  response.sendFile(path.join(__dirname, "src/server/page.html"));
 });
 
 function keepServerRunning() {
