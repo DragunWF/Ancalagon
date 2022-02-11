48f1b8561c89522efe0c1af2da5a3ab3b3497fa4
DragonWF <plarisanmarc@gmail.com>
2022-02-11 13:03:31 +0800

    Added more embed colors and replaced one

--- src/utils/command.js
+++ src/utils/command.js
@@ -1,16 +1,24 @@
 const { MessageEmbed } = require("discord.js");
 const embedMessageColors = [
-  "#0F52BA", // Blue
-  "#9b111e", // Red
-  "#50C878", // Green
+  "#0F52BA", // Sappire Blue
+  "#9b111e", // Ruby Red
+  "#50C878", // Emerald Green
   "#faed27", // Yellow
   "#E38C2D", // Orange
-  "#A1045A", // Magneta
-  "#9966cc", // Amethyst (Purple)
+  "#A1045A", // Magenta
+  "#9966cc", // Purple Amethyst
   "#FFC0CB", // Pink
   "#D5F6FB", // Aqua
   "#9BD087", // Pastel Green
-  "#0000FF", // Cyan
+  "#A4D8D8.", // Pastel Cyan
+  "#FDFD96", // Pastel Yellow
+  "#836953", // Pastel Brown
+  "#AEC6CF", // Pastel Blue
+  "#C3B1E1", // Pastel Purple
+  "#FAC898", // Pastel Orange
+  "#f49ac2", // Pastel Magenta
+  "#FOB6D5", // Pastel Pink
+  "#E39FF6", // Lavender
 ];
 
 class Command {
