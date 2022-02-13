384a120fb78db87366eba676f1f4a3e030e45241
DragonWF <plarisanmarc@gmail.com>
2022-02-13 23:43:08 +0800

    Initialized file and base class

---
+++ src/commands/crypto.js
@@ -0,0 +1,10 @@
+import fetch from "node-fetcj";
+import Command from "../utils/command.js";
+
+class CryptoCommand extends Command {
+  constructor() {
+    super();
+  }
+}
+
+export default CryptoCommand;
