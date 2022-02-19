7482082a989ca293316f7794a304b884baca5fac
DragonWF <plarisanmarc@gmail.com>
2022-02-20 01:00:58 +0800

    Syntax fixes and optimizations

--- src/commands/economy.js
+++ src/commands/economy.js
@@ -1,12 +1,11 @@
 import Command from "../utils/command.js";
 import Economy from "../utils/economy_handler.js";
 
-let data = null;
-let dataIndex = null;
-
 class EconomyCommand extends Command {
   constructor() {
     super();
+    this.data = null;
+    this.dataIndex = null;
   }
 
   scavenge(message) {
@@ -25,46 +24,44 @@ class EconomyCommand extends Command {
 
   balance(object, message) {
     Economy.checkUser(message);
-    data = Economy.readEconomyData(true);
-    console.log(data);
-    dataIndex = Economy.getDataIndex(message.author.id);
-    const value = data[dataIndex].coins
+    object.data = Economy.readEconomyData(true);
+    object.dataIndex = Economy.getDataIndex(message.author.id);
+    const value = object.data[object.dataIndex].coins
       .toFixed(2)
       .toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     const embedOutput = new object.MessageEmbed()
       .setColor(object.getRandomEmbedColor())
-      .setTitle("Coin Balance")
       .setAuthor({
         name: message.author.tag,
         iconURL: message.author.avatarURL(),
       })
-      .setDescription(`Your Coin Amount: **${value}** 💷`)
+      .setDescription(`Coin Balance: **${value}** 💷`)
       .setFooter({ text: "Dragon's Economy" })
       .setTimestamp();
     return { embeds: [embedOutput] };
   }
 
   leaderboard(object, message) {
-    data = Economy.readEconomyData(true);
+    object.data = Economy.readEconomyData(true);
     const dataSets = [];
-    for (let userData of data)
+    for (let userData of object.data)
       dataSets.push({ tag: userData.tag, coins: userData.coins });
     dataSets.sort((a, b) => {
       return a.coins - b.coins;
     });
 
-    const output = "";
+    let output = "";
     for (let i = 0; i < dataSets.length; i++) {
       output += `**#${i + 1}:** ${dataSets[i].tag} - \`${
         dataSets[i].coins
-      } coins\``;
+      } coins\`\n`;
       if (i + 1 >= 10) break;
     }
 
     const embedOutput = new object.MessageEmbed()
       .setColor(object.getRandomEmbedColor())
-      .setTitle("Leaderboard in Dragon's Economy")
+      .setTitle("Leaderboard In Dragon's Economy")
       .setDescription(output)
       .setFooter({ text: "Richest users in Dragon's Economy" })
       .setTimestamp();
